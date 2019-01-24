9e00455429964b2b344c0e318d3225d5f560addd
henarons <henrik.brautaset.aronsen@finn.no>
2019-01-24 17:38:28 +0100

    Some more wip for git auth

--- app/src/main/java/no/synth/revertalicious/MainActivity.kt
+++ app/src/main/java/no/synth/revertalicious/MainActivity.kt
@@ -1,29 +1,15 @@
 package no.synth.revertalicious
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.api.TransportConfigCallback
-import org.eclipse.jgit.transport.JschConfigSessionFactory
-import org.eclipse.jgit.transport.OpenSshConfig.Host
-import org.eclipse.jgit.transport.SshSessionFactory
-import org.eclipse.jgit.transport.SshTransport
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
-import org.eclipse.jgit.util.FS
+import no.synth.revertalicious.GitTask.Companion.AuthenticationMethod
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,21 +21,19 @@ class MainActivity : AppCompatActivity() {
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
 
         val preferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val settingsAuthMethod = preferences.getString(SettingsActivity.AUTH_METHOD, null)?.let { AuthenticationMethod.parse(it) }
-        val settingsRepoUrl = preferences.getString(SettingsActivity.RESPOSITORY, null)?.let { Uri.parse(it) }
+        val settingsAuthMethod = AuthenticationMethod.parse(preferences.getString(SettingsActivity.AUTH_METHOD, null))
+        val settingsRepoUrl = preferences.getString(SettingsActivity.RESPOSITORY, null) ?: ""
         val settingsPassword = preferences.getString(SettingsActivity.PASSWORD, null)
         val settingsPrivateKey = preferences.getString(SettingsActivity.PRIVATE_KEY, null)
 
-        Toast.makeText(this, "Auth: $settingsAuthMethod Repo: $settingsRepoUrl Password: $settingsPassword Key: ${settingsPrivateKey != null}", Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this,
+            "Auth: $settingsAuthMethod Repo: $settingsRepoUrl Password: $settingsPassword Key: ${settingsPrivateKey != null}",
+            Toast.LENGTH_LONG
+        ).show()
 
-        revert.setOnClickListener { view ->
-            GitTask(
-                settingsRepoUrl,
-                settingsPassword,
-                settingsPrivateKey,
-                settingsAuthMethod,
-                this
-            ).execute()
+        revert.setOnClickListener { _ ->
+            GitTask(settingsRepoUrl, settingsPassword, settingsPrivateKey, settingsAuthMethod,this).execute()
         }
     }
 
@@ -73,112 +57,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-private enum class AuthenticationMethod {
-    password, pubkey, token;
-
-    companion object {
-        fun parse(value: String): AuthenticationMethod? =
-            AuthenticationMethod.values().find { it.name == value.toLowerCase() }
-    }
-}
-
-private class GitTask(
-    val repoUrl: Uri?,
-    val password: String?,
-    val sshPrivateKey: String?,
-    val authMethod: AuthenticationMethod?,
-    val context: Context
-) : AsyncTask<String, Int, Unit>() {
-
-    override fun doInBackground(vararg params: String) {
-
-        try {
-            val localDir = context.filesDir.resolve(
-                repoUrl.toString().substring(repoUrl.toString().lastIndexOf("/") + 1)
-            )
-
-            localDir.deleteRecursively()
-
-            val gitBuilder = Git.cloneRepository()
-                .setURI(repoUrl.toString())
-                .setDirectory(localDir)
-
-            when (authMethod) {
-                AuthenticationMethod.pubkey ->
-                    sshPrivateKey?.let {
-                        if (!it.contains("BEGIN RSA ")) {
-                            throw IllegalArgumentException("You need an older style openssh key, created with 'ssh-keygen -t rsa -m PEM'")
-                        }
-                        gitBuilder.setTransportConfigCallback(sshTransportCallback(localDir.name, it, password)).call()
-                    } ?: throw IllegalArgumentException("Missing sshPrivateKey")
-
-                AuthenticationMethod.password -> {
-                    val username = repoUrl?.userInfo
-                    if (password != null && username != null) {
-                        gitBuilder.setCredentialsProvider(UsernamePasswordCredentialsProvider(username, password))
-                    } else {
-                        throw IllegalArgumentException("Missing username or password")
-                    }
-                }
-
-                AuthenticationMethod.token ->
-                    password?.let {
-                        gitBuilder.setCredentialsProvider(UsernamePasswordCredentialsProvider("token", it))
-                    } ?: throw IllegalArgumentException("Missing password")
-            }
-
-            val git = gitBuilder.call()
-
-            System.out.println("før")
-
-            git.log().call().forEach {
-                System.out.println(it.fullMessage)
-            }
-            git.revert().include(git.log().call().first()).call()
-
-            System.out.println("etter")
-            git.log().call().forEach {
-                System.out.println(it.fullMessage)
-            }
-            git.push().setDryRun(true).call()
-        } catch (e: Exception) {
-
-            (context as Activity).runOnUiThread(object : Runnable {
-                override fun run() {
-                    Toast.makeText(context, "Error: " + e.toString(), Toast.LENGTH_SHORT).show()
-                    e.printStackTrace()
-                }
-            })
-        }
-    }
-
-    companion object {
-
-        fun sshTransportCallback(name: String, privKey: String, password: String? = null): TransportConfigCallback {
-
-            val sshSessionFactory: SshSessionFactory = object : JschConfigSessionFactory() {
-
-                override fun configure(host: Host, session: Session) = Unit
-
-                override fun createDefaultJSch(fs: FS): JSch {
-                    val defaultJSch = super.createDefaultJSch(fs)
-                    defaultJSch.addIdentity(
-                        name,
-                        privKey.toByteArray(Charsets.UTF_8),
-                        null,
-                        password?.toByteArray(Charsets.UTF_8)
-                    )
-                    JSch.setConfig("StrictHostKeyChecking", "no");
-                    return defaultJSch
-                }
-            }
-
-            return TransportConfigCallback {
-                val sshTransport: SshTransport = it as SshTransport
-                sshTransport.sshSessionFactory = sshSessionFactory
-            }
-        }
-    }
-}
-
