b8008ad938ff7d241732c2c86eda3d27c88d6129
henrik242 <henrik242@users.noreply.github.com>
2019-01-25 12:32:30 +0100

    Small cleanup

--- app/src/main/java/no/synth/revertalicious/GitTask.kt
+++ app/src/main/java/no/synth/revertalicious/GitTask.kt
@@ -12,6 +12,7 @@ import no.synth.revertalicious.auth.AuthenticationMethod.pubkey
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.TransportCommand
 import org.eclipse.jgit.api.TransportConfigCallback
+import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.transport.*
 import org.eclipse.jgit.util.FS
 import java.io.File
@@ -25,50 +26,46 @@ class GitTask(
     val context: Context
 ) : AsyncTask<String, Int, Unit>() {
 
-    val gitUrl: GitUrl
-
-    init {
-        gitUrl = resolveUrl(repoUrl)
-    }
-
     public override fun doInBackground(vararg params: String) {
 
         try {
-            val localDir = resolveDir(context.filesDir, repoUrl)
-            deleteRecursive(localDir)
-
-            val cloneCmd = Git.cloneRepository()
-                .setURI(repoUrl)
-                .setDirectory(localDir)
-
-            deleteRecursive(localDir)
-
-            authenticate(cloneCmd)
-            val git = cloneCmd.call()
+            val git = clone()
 
             System.out.println("før")
 
-            git.log().call().forEach {
-                System.out.println(it.fullMessage)
-            }
-            git.revert().include(git.log().call().first()).call()
+            printLastTen(git)
+            revertLast(git)
 
             System.out.println("etter")
-            git.log().call().forEach {
-                System.out.println(it.fullMessage)
-            }
 
+            printLastTen(git)
             push(git)
 
         } catch (e: Exception) {
             if (context is Activity) {
                 context.runOnUiThread {
-                    Toast.makeText(context, "Error: " + e.toString(), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Error: $e", Toast.LENGTH_SHORT).show()
                 }
             } else {
                 throw e
             }
         }
+    }
+
+    private fun printLastTen(git: Git) {
+        git.log().call().forEach {
+            System.out.println(it.fullMessage)
+        }
+    }
+
+    private fun revertLast(git: Git) : RevCommit = git.revert().include(git.log().call().first()).call()
+
+    private fun clone(): Git {
+        val localDir = resolveDir(context.filesDir, repoUrl)
+        deleteRecursive(localDir)
+        val cloneCmd = Git.cloneRepository().setURI(repoUrl).setDirectory(localDir)
+        authenticate(cloneCmd)
+        return cloneCmd.call()
     }
 
     private fun push(git: Git): Iterable<PushResult> {
@@ -83,7 +80,7 @@ class GitTask(
                 if (!it.contains("BEGIN RSA ")) {
                     throw IllegalArgumentException("You need an older style openssh key, created with 'ssh-keygen -t rsa -m PEM'")
                 }
-                cmd.setTransportConfigCallback(sshTransportCallback(context, gitUrl, it, passwd)).call()
+                cmd.setTransportConfigCallback(sshTransportCallback())
             } ?: throw IllegalArgumentException("Missing sshPrivateKey")
 
         password -> {
@@ -95,80 +92,47 @@ class GitTask(
         }
     }
 
-    companion object {
+    private fun sshTransportCallback(): TransportConfigCallback {
 
+        fun sshSessionFactory() = object : JschConfigSessionFactory() {
 
-        fun resolveUrl(repoUrl: String): GitUrl =
-            when {
-                repoUrl.matches(gitUrlPattern) -> gitUrlPattern.find(repoUrl)?.groupValues
-                else -> regularUrlPattern.find(repoUrl)?.groupValues
-            }?.let {
-                GitUrl(
-                    user = if (it[1].isNotEmpty()) it[1].replace("@", "") else null,
-                    host = it[2],
-                    dir = it[3]
+            override fun configure(host: OpenSshConfig.Host, session: Session) {
+                session.setConfig("StrictHostKeyChecking", "no");
+            }
+
+            override fun createDefaultJSch(fs: FS): JSch {
+                //val jsch = super.createDefaultJSch(fs)
+                val jsch = JSch()
+                jsch.setKnownHosts(context.resources.openRawResource(R.raw.known_hosts))
+                jsch.addIdentity(
+                    repoUrl,
+                    sshPrivateKey?.toByteArray(Charsets.UTF_8),
+                    null,
+                    passwd?.toByteArray(Charsets.UTF_8)
                 )
-            } ?: throw IllegalArgumentException("Cannot parse url: $repoUrl")
+                return jsch
+            }
+        }
+
+        return TransportConfigCallback {
+            val sshTransport: SshTransport = it as SshTransport
+            sshTransport.sshSessionFactory = sshSessionFactory()
+        }
+    }
+
+    companion object {
 
         private fun resolveDir(filesDir: File, repoUrl: String): File =
             filesDir.resolve(repoUrl.substring(repoUrl.lastIndexOf("/") + 1))
 
-        fun sshTransportCallback(context: Context, gitUrl: GitUrl, privKey: String, password: String? = null): TransportConfigCallback {
 
-            fun sshSessionFactory() = object : JschConfigSessionFactory() {
-
-                override fun configure(host: OpenSshConfig.Host, session: Session) {
-                    session.setConfig("StrictHostKeyChecking", "no");
-                }
-
-                override fun createDefaultJSch(fs: FS): JSch {
-                    //val jsch = super.createDefaultJSch(fs)
-                    val jsch = JSch()
-                    jsch.setKnownHosts(context.resources.openRawResource(R.raw.known_hosts))
-
-//                    jsch.hostKeyRepository = object : HostKeyRepository {
-//                        override fun check(host: String?, key: ByteArray?): Int = HostKeyRepository.OK
-//                        override fun add(hostkey: HostKey?, ui: UserInfo?) {}
-//                        override fun getHostKey(): Array<HostKey> = arrayOf()
-//                        override fun getHostKey(host: String?, type: String?): Array<HostKey> = arrayOf()
-//                        override fun remove(host: String?, type: String?) {}
-//                        override fun remove(host: String?, type: String?, key: ByteArray?) {}
-//                        override fun getKnownHostsRepositoryID(): String = "someId"
-//                    }
-                    jsch.addIdentity(
-                        gitUrl.host,
-                        privKey.toByteArray(Charsets.UTF_8),
-                        null,
-                        password?.toByteArray(Charsets.UTF_8)
-                    )
-                    return jsch
-                }
+        private fun deleteRecursive(fileOrDirectory: File) {
+            if (fileOrDirectory.isDirectory) {
+                for (child in fileOrDirectory.listFiles()) deleteRecursive(child)
             }
-
-            return TransportConfigCallback {
-                val sshTransport: SshTransport = it as SshTransport
-                sshTransport.sshSessionFactory = sshSessionFactory()
-            }
-        }
-
-        fun deleteRecursive(fileOrDirectory: File) {
-            if (fileOrDirectory.isDirectory)
-                for (child in fileOrDirectory.listFiles())
-                    deleteRecursive(child)
-
             fileOrDirectory.delete()
         }
-
-        val gitUrlPattern = Regex("^(\\w+)@([.\\w]+):.*/(.*?)$")
-        val regularUrlPattern = Regex("^\\w+://(\\w*@)?(.*?)/.*?([^/]+)$")
     }
 }
-
-data class GitUrl(
-    val user: String? = null,
-    val host: String,
-    val dir: String
-)
-
 
 
--- app/src/test/java/no/synth/revertalicious/GitTaskTest.kt
+++ app/src/test/java/no/synth/revertalicious/GitTaskTest.kt
@@ -1,20 +1,13 @@
 package no.synth.revertalicious
 
 import android.content.Context
-import no.synth.revertalicious.GitTask.Companion.resolveUrl
-import org.junit.Assert.assertEquals
+import no.synth.revertalicious.auth.AuthenticationMethod
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
-
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-
+import java.io.File
 
 @RunWith(MockitoJUnitRunner::class)
 class GitTaskTest {
@@ -23,28 +16,26 @@ class GitTaskTest {
     private lateinit var mockContext: Context
 
     @Test
-    fun can_parse_git_url() {
-        val url = resolveUrl("git@github.com:henrik242/testing123.git")
-        assertEquals(GitUrl("git", "github.com", "testing123.git"), url)
+    fun can_ssh_auth() {
+        val tmpDir = File(System.getProperty("java.io.tmpdir") + System.currentTimeMillis())
+        val passwd = this.javaClass.getResource("/test_passwd").readText()
+        val username = "henrik242"
+
+        try {
+            `when`(mockContext.filesDir).thenReturn(tmpDir)
+
+            val task = GitTask(
+                "https://github.com/henrik242/testing123.git",
+                username,
+                passwd,
+                null,
+                AuthenticationMethod.password,
+                mockContext
+            )
+            task.doInBackground()
+
+        } finally {
+            tmpDir.deleteRecursively()
+        }
     }
-
-    @Test
-    fun can_parse_http_url() {
-        val url = resolveUrl("https://foo@github.com/henrik242/testing123.git")
-        assertEquals(GitUrl("foo", "github.com", "testing123.git"), url)
-    }
-
-    @Test
-    fun can_parse_http_url_without_user() {
-        val url = resolveUrl("https://github.com/henrik242/testing123.git")
-        assertEquals(GitUrl(null, "github.com", "testing123.git"), url)
-    }
-
-    @Test
-    fun can_parse_ssh_url() {
-        val url = resolveUrl("ssh://foo@github.com/henrik242/testing123.git")
-        assertEquals(GitUrl("foo", "github.com", "testing123.git"), url)
-    }
-
-
 }
