ea055f6943bc44932f4eb63bbf5f0b34b0bef912
henrik242 <henrik242@users.noreply.github.com>
2019-10-17 13:16:23 +0200

    Migrate to kotlin-dsl, step 2
    Ref https://proandroiddev.com/migrating-android-build-scripts-from-groovy-to-kotlin-dsl-f8db79dd6737
    Fixes #3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,44 @@
-apply plugin: 'com.android.application'
-
-apply plugin: 'kotlin-android'
-
-apply plugin: 'kotlin-android-extensions'
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
 
 android {
-    compileSdkVersion 28
+    compileSdkVersion(28)
     defaultConfig {
-        applicationId "no.synth.revertalicious"
-        minSdkVersion 26
-        targetSdkVersion 28
-        versionCode 1
-        versionName "1.0"
-        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        applicationId = "no.synth.revertalicious"
+        minSdkVersion(26)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
-        release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     packagingOptions {
-        exclude 'META-INF/DEPENDENCIES'
+        exclude("META-INF/DEPENDENCIES")
     }
 }
 
 dependencies {
-    implementation fileTree(dir: 'libs', include: ['*.jar'])
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
-    implementation 'androidx.appcompat:appcompat:1.1.0'
-    implementation 'androidx.preference:preference:1.1.0'
-    implementation 'androidx.constraintlayout:constraintlayout:1.1.3'
-    implementation 'com.google.android.material:material:1.0.0'
-    implementation 'org.eclipse.jgit:org.eclipse.jgit:4.5.5.201812240535-r' // 4.5.x is the last with JDK7 support
-    implementation 'com.google.firebase:firebase-core:17.2.0'
+    implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.preference:preference:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:4.5.5.201812240535-r") // 4.5.x is the last with JDK7 support
+    implementation("com.google.firebase:firebase-core:17.2.0")
 
-    testImplementation 'junit:junit:4.12'
-    testImplementation 'androidx.test:core:1.2.0'
-    testImplementation 'org.mockito:mockito-core:3.1.0'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.1'
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.2.0'
+    testImplementation("junit:junit:4.12")
+    testImplementation("androidx.test:core:1.2.0")
+    testImplementation("org.mockito:mockito-core:3.1.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
-apply plugin: 'com.google.gms.google-services'
+apply(plugin = "com.google.gms.google-services")
