5900062868ae6f4b136a7221367e49ab7ce735d8
henrik242 <henrik242@users.noreply.github.com>
2021-03-11 11:45:21 +0100

    Add some secrets

--- app/src/test/java/no/synth/revertalicious/GitTaskTest.kt
+++ app/src/test/java/no/synth/revertalicious/GitTaskTest.kt
@@ -18,15 +18,28 @@ class GitTaskTest {
 
     @Test
     fun can_ssh_auth() {
-        val tmpDir = File(System.getProperty("java.io.tmpdir")?.let { it + System.currentTimeMillis() })
-        val passwd = this.javaClass.getResource("/test_passwd")?.readText()
-        val username = "henrik242"
+        val tmpDir = File((System.getProperty("java.io.tmpdir") ?: "/tmp") + System.currentTimeMillis())
+        val passwd = if (System.getProperty("GITTASKTEST_PASS").isNullOrBlank()) {
+            this.javaClass.getResource("/gittasktest_pass")?.readText()
+        } else {
+            System.getProperty("GITTASKTEST_PASS")
+        }
+        val username = if (System.getProperty("GITTASKTEST_USER").isNullOrBlank()) {
+            "henrik242"
+        } else {
+            System.getProperty("GITTASKTEST_USER")
+        }
+        val repoUrl = if (System.getProperty("GITTASKTEST_REPO").isNullOrBlank()) {
+            "https://github.com/henrik242/testing123.git"
+        } else {
+            System.getProperty("GITTASKTEST_REPO")
+        }
 
         try {
             mwhen(mockContext.filesDir).thenReturn(tmpDir)
 
             val task = object : GitTask(
-                "https://github.com/henrik242/testing123.git",
+                repoUrl,
                 username,
                 passwd,
                 null,
