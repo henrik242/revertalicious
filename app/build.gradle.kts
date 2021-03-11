c4444ea2b2e54cde223c37c59f3f0a68882bf246
henrik242 <henrik242@users.noreply.github.com>
2021-03-11 11:01:42 +0100

    upgrade deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdkVersion(28)
+    compileSdkVersion(30)
     defaultConfig {
         applicationId = "no.synth.revertalicious"
         minSdkVersion(26)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -27,18 +27,18 @@ android {
 
 dependencies {
     implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.preference:preference:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("com.google.android.material:material:1.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.4.31")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.android.material:material:1.3.0")
     implementation("org.eclipse.jgit:org.eclipse.jgit:4.5.5.201812240535-r") // 4.5.x is the last with JDK7 support
-    implementation("com.google.firebase:firebase-core:17.2.0")
+    implementation("com.google.firebase:firebase-core:18.0.2")
 
-    testImplementation("junit:junit:4.12")
-    testImplementation("androidx.test:core:1.2.0")
-    testImplementation("org.mockito:mockito-core:3.1.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test:core:1.3.0")
+    testImplementation("org.mockito:mockito-core:3.8.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
 apply(plugin = "com.google.gms.google-services")
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,12 @@ buildscript {
 
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
 
-        classpath("com.google.gms:google-services:4.3.2")
+        classpath("com.google.gms:google-services:4.3.5")
     }
 }
 
